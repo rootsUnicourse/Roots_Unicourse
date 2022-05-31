09a257f856d996267bb898dabf18c6c3d0d54e83
unicourse <rootsunicourse@gmail.com>
2022-05-31 15:13:21 +0300

    remove video

--- roots/src/components/Content/Content.js
+++ roots/src/components/Content/Content.js
@@ -39,7 +39,7 @@ const Content = () => {
 
     return (
         <Container>
-            <CardMedia className={classes.space} component="video" controls src={noga}/>
+            <CardMedia className={classes.space} component="video" controls />
             <Grid container spacing={2} className={classes.gridSpace}>
                 <Grid item lg={6} sm={6} xs={6}><Card className={classes.longCard}>הסבר</Card></Grid>
                 <Grid item lg={6} sm={6} xs={6}><Card className={classes.longCard}>עוד משהו</Card></Grid>
